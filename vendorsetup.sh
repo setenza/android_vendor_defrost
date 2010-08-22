945d62e571ae7029d90a3b946d2a9c1b374e3a88
richardtrip <richard.trip@gmail.com>
2010-08-22 15:46:13 +0200

    added defrost to vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,3 @@
-add_lunch_combo cyanogen_passion-eng
-add_lunch_combo cyanogen_inc-eng
-add_lunch_combo cyanogen_hero-eng
-add_lunch_combo cyanogen_heroc-eng
-add_lunch_combo cyanogen_sholes-eng
-add_lunch_combo cyanogen_dream_sapphire-eng
-add_lunch_combo cyanogen_bravo-eng
-add_lunch_combo cyanogen_espresso-eng
-add_lunch_combo cyanogen_supersonic-eng
-add_lunch_combo cyanogen_liberty-eng
-add_lunch_combo cyanogen_vibrant-eng
+add_lunch_combo defrost-eng
 
 PATH=$PATH:$PWD/vendor/cyanogen/tools ; export PATH
